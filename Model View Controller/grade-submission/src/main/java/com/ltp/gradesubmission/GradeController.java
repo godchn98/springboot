79abdc14ecbd61ac508314e03b8d233afe896a94
Yeojin Park <godchn98@gmail.com>
2023-01-12 13:23:02 +0700

    submit form added
    submit form added using postmapping and redirect

--- Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -6,21 +6,30 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Harry", "Potions", "C-"),
-        new Grade("Hermione", "Spelling", "A+"),
-        new Grade("Ron", "Charms", "C+")
-    ); 
+    List<Grade> studentGrades = new ArrayList<>();
+
+    @GetMapping("/")
+    public String gradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade){
+        studentGrades.add(grade);
+        return "redirect:/grades";
+    }
 
     @GetMapping("/grades")
-    public String getGrades(Model model) {
-
+    public String getGrades(Model model){
         model.addAttribute("grades", studentGrades);
         return "grades";
     }
+
 }

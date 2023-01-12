27f1c0b39f4ada351e3288e8f42d4993b2e1878e
Yeojin Park <godchn98@gmail.com>
2023-01-12 12:37:58 +0700

    workbook2.10 solved

--- Model View Controller/workbook2.10/src/main/java/com/ltp/workbook/WorkbookController.java
+++ Model View Controller/workbook2.10/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -7,13 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ltp.Employee;
+
 
 @Controller
 public class WorkbookController {
 
+    List<Employee> employees = Arrays.asList(
+        new Employee("Jim Halpert", 32, "Salesman"), 
+        new Employee("Andy Bernard", 38, "Salesman"),
+        new Employee("Pam Beesly", 32, "Receptionist"),
+        new Employee("Michael Scott", 49, "Reginal MManager"),
+        new Employee("Ryan Howard", 28, "Temp"),
+        new Employee("Angela Martin", 35, "Accountant"),
+        new Employee("Dwight Schrute", 37, "Assistant to the Regional Manager")
+    );
     
     @GetMapping(value="/")
     public String getStaff(Model model) {
+        model.addAttribute("staff", employees);
         return "staff";
     }
     

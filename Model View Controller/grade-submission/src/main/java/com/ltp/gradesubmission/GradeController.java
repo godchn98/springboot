b02cae2bde85b3c4de10f52b97858bb42d4f242c
Yeojin Park <godchn98@gmail.com>
2023-01-12 12:06:11 +0700

    Grade-submission updated using thymeleaf loop

--- Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,5 +1,9 @@
 package com.ltp.gradesubmission;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -7,10 +11,16 @@ import org.springframework.ui.Model;
 @Controller
 public class GradeController {
 
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade("Harry", "Potions", "C-"),
+        new Grade("Hermione", "Spelling", "A+"),
+        new Grade("Ron", "Charms", "C+")
+    ); 
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("Harry", "Potions", "C-");
-        model.addAttribute("grade", grade);
+
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }

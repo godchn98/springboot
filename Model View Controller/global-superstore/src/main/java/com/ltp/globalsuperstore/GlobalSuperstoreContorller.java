7dc8b319500bc118322116e56142c8fe857cc4ce
Yeojin Park <godchn98@gmail.com>
2023-01-13 11:45:01 +0700

    updated success alert added

--- Model View Controller/global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreContorller.java
+++ Model View Controller/global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreContorller.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class GlobalSuperstoreContorller {
@@ -24,13 +25,14 @@ public class GlobalSuperstoreContorller {
     }
 
     @PostMapping("/submitItem")
-    public String handleSubmit(Item item) {
+    public String handleSubmit(Item item, RedirectAttributes redirectAttributes) {
         int index = getIndexFromID(item.getId());
         if (index == Constants.NOT_FOUND) {
             items.add(item);
         } else {
             items.set(index, item);
         }
+        redirectAttributes.addFlashAttribute("status", Constants.SUCCESS_STATUS);
         return "redirect:/inventory";
     }
 
